66400334dabb16d5e5db0cfa5361843295fa3453
Olly Shenton <oliver.shenton@analogfolk.com>
2021-02-18 22:36:39 +0000

    Initial commit

---
+++ src/components/ButtonLink.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <a
+      v-if="!disabled"
+      :href="url"
+      :class="`${variant} ${disabled}`"
+      :disabled="disabled"
+    >
+      <span v-if="icon.length" class="button--icon">
+        <slot name="svgContent"></slot>
+      </span>
+      <slot>{{ text }}</slot>
+    </a>
+    <div
+      v-else
+      :class="`${variant}`"
+      :disabled="disabled"
+      class="disabled true"
+      aria-label="disabled button"
+    >
+      <span v-if="icon.length" class="button--icon">
+        <slot name="svgContent"></slot>
+      </span>
+      <slot>{{ text }}</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonLink",
+  props: {
+    url: {
+      type: String,
+      default: "url"
+    },
+    text: {
+      type: String,
+      required: true,
+      default: "Button"
+    },
+    variant: {
+      type: String,
+      default: "button"
+    },
+    icon: {
+      type: String,
+      required: false,
+      default: "icon"
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.button,
+button {
+  color: $white;
+  text-align: center;
+  cursor: pointer;
+  text-decoration: none;
+  padding: rem(12.5px) rem(22px);
+  font-size: $font-size-body;
+  font-family: $font-family;
+  font-weight: $font-weight-reg;
+  display: inline-flex;
+  line-height: 1.1;
+
+  span {
+    padding-right: rem(10px);
+  }
+  svg {
+    fill: $white;
+    height: rem(18px);
+  }
+}
+.button--icon {
+  height: rem(18px);
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,55 @@
+<template>
+  <Layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>Hello, world!</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+      excepturi labore tempore expedita, et iste tenetur suscipit explicabo!
+      Dolores, aperiam non officia eos quod asperiores
+    </p>
+
+    <Button
+      url="http://www.brixtoncycles.com"
+      text="Button Test"
+      variant="primary"
+    />
+    <Card />
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener"
+        >Gridsome Docs</a
+      >
+      <a
+        href="https://github.com/gridsome/gridsome"
+        target="_blank"
+        rel="noopener"
+        >GitHub</a
+      >
+    </p>
+  </Layout>
+</template>
+
+<script>
+import Button from "~/components/ButtonLink.vue";
+import Card from "~/components/Card.vue";
+
+export default {
+  name: "index",
+  components: {
+    Card,
+    Button
+  },
+  metaInfo: {
+    title: "Hello, test page"
+  }
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
